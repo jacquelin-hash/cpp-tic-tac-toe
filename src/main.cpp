c2c0a0ff4cbd08355ca1d9c43176c65946b93b93
Jacqueline Guimac <jacquelineguimac@gmail.com>
2026-07-27 18:47:52 -0400

    Deleted

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     cout << "Hello, tic-tac-toe!" << endl;
-    return 0;
+    return 0
 }
 
 void printBoard(){
@@ -11,4 +11,3 @@ void printBoard(){
     // loop through columns 
     // print each cell, with some specator between columns 
     // print a line between rows ( "like ---")
-}
