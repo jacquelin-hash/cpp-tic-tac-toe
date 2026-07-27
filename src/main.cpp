f61e83b6f3afa3fb6276f037c664a8991e5674f7
Jacqueline Guimac <71201628+jacquelin-hash@users.noreply.github.com>
2026-07-28 02:11:49 +0300

    New function (#2)
    * New function
    
    * Deleted
    
    * Fixed

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,10 @@ int main() {
     cout << "Hello, tic-tac-toe!" << endl;
     return 0;
 }
+
+void printBoard(){
+    // loop through rows 
+    // loop through columns 
+    // print each cell, with some specator between columns 
+    // print a line between rows ( "like ---")
+}
